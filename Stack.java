b7583c91a11a7b64bef5e9adeaec3cdd60e680ce
gdadulla <gdadulla@albany.edu>
2017-04-04 17:01:54 -0400

    adds code for push() and isEmpty() methods

--- Stack.java
+++ Stack.java
@@ -3,13 +3,20 @@ package Homework4;
 public class Stack {
 	
 	private Node top = null;
+
 	
-	public void push(){
-		
+	public void push(Node n){
+		if(this.isEmpty()){
+			this.top = n;
+		}
+		else{
+			n.setNext(top);
+			this.top = n;
+		}
 	}
 	
 	public boolean isEmpty(){
-		
+		return this.top == null;
 	}
 	
 	public Node pop(){
