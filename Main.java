5064f6b1ee0b1b64d2b79dbd44859e6e623819a1
gdadulla <gdadulla@albany.edu>
2017-04-06 14:08:08 -0400

    adds test code in main class

--- Main.java
+++ Main.java
@@ -1,7 +1,30 @@
 package Homework4;
 
+
 public class Main {
-
-
 	
+	public static void main (String [] args){
+		Stack s = new Stack();
+		Queue q = new Queue();
+		Node node = new Node("Germaine");
+		Node node2 = new Node("Mike");
+		Node node3 = new Node("Jamell");
+		Node node4 = new Node("Vinny");
+		
+		s.push(node);
+		s.push(node2);
+		s.push(node3);
+		s.push(node4);
+		s.print();
+		System.out.println("");
+		
+		s.pop();
+		s.print();
+		
+		q.enqueue(node);
+		q.enqueue(node2);
+		q.enqueue(node3);
+		q.enqueue(node4);
+		q.print();
+		}
 }
